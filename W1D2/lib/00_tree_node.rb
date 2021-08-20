20587cc76c7339489b23b6e16d9edb240dd7f6f6
Axeia <pbakhuis@gmail.com>
2021-08-20 15:39:02 +0100

    Implemented depth and breath search functionality and met all of the rspec specs. (Did have to peek at the solution file)

--- W1D2/lib/00_tree_node.rb
+++ W1D2/lib/00_tree_node.rb
@@ -1,4 +1,39 @@
+module Searchable
+    def dfs(target = nil, &prc)
+        raise "Need a proc or a target" if [target, prc].none?
+        prc ||= Proc.new{ |node| node.value == target }
+
+        return self if prc.call(self)
+
+        children.each do |child| 
+            result = child.dfs(&prc)
+            return result unless result.nil?
+        end
+        
+        nil
+    end
+
+    def bfs(target = nil, &prc)
+        raise "Need a proc or target" if [target, prc].none?
+        prc ||= Proc.new { |node| node.value == target }
+        nodes = [self]
+
+        until nodes.empty?
+            node = nodes.shift
+            return node if prc.call(node)
+            nodes = nodes + node.children
+        end
+
+        nil
+    end
+
+    def count
+        1 + children.map{ |child| child.count }.to_a.sum
+    end
+end
+
 class PolyTreeNode
+    include Searchable
     attr_accessor :value
     attr_reader :parent
 
